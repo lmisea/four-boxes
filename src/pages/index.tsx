c496cf68f53b9532edaab3ef83efeaf68b55988a
Luis Isea <19-10175@usb.ve>
2023-12-17 08:17:16 -0400

    Added the four pin input boxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,19 @@ import { BlitzPage } from "@blitzjs/next"
 import Layout from "src/core/layouts/Layout"
 import styles from "src/styles/Verification.module.css"
 
-import { Box, Card, CardBody, CardHeader, Heading, Link, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  HStack,
+  Heading,
+  Link,
+  PinInput,
+  PinInputField,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
 
 const CodeVerification: BlitzPage = () => {
   return (
@@ -15,9 +27,51 @@ const CodeVerification: BlitzPage = () => {
             <Heading as="h1">Enter the code we emailed you.</Heading>
           </CardHeader>
 
-          <CardBody color="#CEC9D0">
-            <Box textAlign="right">
-              <Text as="b">
+          <CardBody>
+            <VStack>
+              <HStack spacing="5">
+                <PinInput
+                  size="lg"
+                  placeholder=""
+                  colorScheme="whiteAlpha"
+                  variant="filled"
+                  focusBorderColor="#5684D4"
+                  autoFocus={true}
+                >
+                  <PinInputField
+                    _focus={{ backgroundColor: "#E2E8F0", borderWidth: "3px" }}
+                    fontWeight="bolder"
+                    fontSize="2.6em"
+                    minW={"6dvw"}
+                    minH={"11dvh"}
+                  />
+                  <PinInputField
+                    _focus={{ backgroundColor: "#E2E8F0", borderWidth: "3px" }}
+                    fontWeight="bolder"
+                    fontSize="2.6em"
+                    minW={"6dvw"}
+                    minH={"11dvh"}
+                  />
+                  <PinInputField
+                    _focus={{ backgroundColor: "#E2E8F0", borderWidth: "3px" }}
+                    fontWeight="bolder"
+                    fontSize="2.6em"
+                    minW={"6dvw"}
+                    minH={"11dvh"}
+                  />
+                  <PinInputField
+                    _focus={{ backgroundColor: "#E2E8F0", borderWidth: "3px" }}
+                    fontWeight="bolder"
+                    fontSize="2.6em"
+                    minW={"6dvw"}
+                    minH={"11dvh"}
+                  />
+                </PinInput>
+              </HStack>
+            </VStack>
+
+            <Box className={styles.email_text}>
+              <Text>
                 adam@gmail.com
                 <br />
                 <Link color="#5684D4">resend</Link>
